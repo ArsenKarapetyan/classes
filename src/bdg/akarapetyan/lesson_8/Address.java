b7a27a4c55340273bf2af80e51d13ecac9d736fd
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-01-25 14:32:41 +0400

    lesson 8

---
+++ src/bdg/akarapetyan/lesson_8/Address.java
@@ -0,0 +1,13 @@
+package akarapetyan.lesson_8;
+
+public class Address {
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address (){
+
+    }
+}
---
+++ src/bdg/akarapetyan/lesson_8/Executor.java
@@ -0,0 +1,12 @@
+package akarapetyan.lesson_8;
+
+public class Executor {
+
+    public static void main(String[] args) {
+
+        Person person = new Person("Arsen", "Karapetyan", "Samvelovich", (short)43);
+        System.out.println(person.toString());
+
+    }
+}
+
