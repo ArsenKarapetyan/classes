3983fc5c96dab094ea31e8445172b235b6eaf266
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-01-25 16:10:09 +0400

    lesson 8

--- src/bdg/akarapetyan/lesson_8/Executor.java
+++ src/bdg/akarapetyan/lesson_8/Executor.java
@@ -5,16 +5,18 @@ public class Executor {
     public static void main(String[] args) {
 
         Address add0 = new Address("Moscovyan", "Yerevan", "Yerevan", "0009");
-        Address add1 = new Address("Moscovyan", "Yerevan", "Yerevan", "0009");
-        Address add2 = new Address("Moscovyan", "Yerevan", "Yerevan", "0009");
+        Address add1 = new Address("Baghramyan", "Yerevan", "Yerevan", "0009");
+        Address add2 = new Address("Saryan", "Yerevan", "Yerevan", "0009");
 
         Address [] add = new Address [3];
         add [0] = add0;
         add [1] = add1;
         add [2] = add2;
 
-        Person person = new Person("Arsen", "Karapetyan", "Samvelovich", (short)43, add);
-        System.out.println(person.toString());
+        Person person1 = new Person("Arsen", "Karapetyan", "Samvelovich", (short)43, add);
+        System.out.println(person1.toString());
+        Person person2 = new Person("Arsen", "Karapetyan", "Samvelovich", (short)25, add);
+        System.out.println(person2.toString());
     }
 }
 
